c806826b6331d5c07fa81d0a11b9fc4bb026906c
OurOh <ahn980321@gmail.com>
2024-07-29 21:16:25 +0900

    first commit 김영한 자바입문

---
+++ src/FitstStep01/HelloJava.java
@@ -0,0 +1,8 @@
+package FitstStep01;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
---
+++ src/variable02/ex/VarEx02.java
@@ -0,0 +1,12 @@
+package variable02.ex;
+
+public class VarEx02 {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
