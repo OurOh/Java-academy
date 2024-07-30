20c7a3516ad6694276bb2e239f717f70c3fbca1d
OurOh <ahn980321@gmail.com>
2024-07-30 22:29:49 +0900

    섹션3 연산자 파트

---
+++ src/Operator03/OperatorEx01.java
@@ -0,0 +1,16 @@
+package Operator03;
+
+public class OperatorEx01 {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        //int는 정수니까 소수점 나머지는 버려진다.
+        int sum = num1 + num2 + num3;
+        int average = (num1 + num2 + num3) / 3;
+
+        System.out.println(sum); //60
+        System.out.println(average); //20
+    }
+}
